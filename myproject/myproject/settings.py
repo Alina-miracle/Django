e862a5bbb8399f060ff05febec7076bbf7b415a0
unlimitedfreedom203 <143114941+unlimitedfreedom203@users.noreply.github.com>
2025-01-31 16:12:33 +0600

    обавлена система пользователей

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
+from .views import register
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),  # Добавляем URL для пользователей
+path('register/', register, name='register'),
+path('accounts/', include('accounts.urls')),
 ]
